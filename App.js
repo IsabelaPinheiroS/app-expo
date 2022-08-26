bda053d8e9b2a8c101626cf48492b80dedafa3ec
unknown <bela_ps@example.com>
2022-08-25 21:09:01 -0300

    primeiro commit

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import {View, Text} from "react-native"; //View e Text são bibliotecas do React Native
+import HomePage from "./src/pages/HomePage";
+
+const App = () => {
+  
+  return (
+
+    <HomePage/> //Aqui, o APP principal está simplemente renderizando a Home Page
+    
+  );
+
+}
+
+export default App;
